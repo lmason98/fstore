37bb7be45b8a15b86f13b1e970ae6ecfd2615576
lmason98 <lmason98@gmail.com>
2024-02-18 17:06:49 -0800

    remove db flush from api entrypoint

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py flush --no-input
+# python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py createsuperuser --no-input
